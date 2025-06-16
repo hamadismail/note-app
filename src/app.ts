4650771ae6a6d031219ba5a466676e168613f0a4
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-16 22:34:06 +0600

    create user controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import { notesRoute } from "./app/controllers/notes.controller";
+import { userRoutes } from "./app/controllers/user.controller";
 
 const app: Application = express();
 app.use(express.json());
 
 app.use('/notes', notesRoute);
+app.use('/user', userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Note App");
