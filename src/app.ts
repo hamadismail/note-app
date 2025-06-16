71d978c609321d3865bfe492ef1a199e2c16e3ce
Hamad Ismail <hamadismail@Hamads-iMac-Pro.local>
2025-06-16 16:52:39 +0600

    implement update single note route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import { model, Schema } from "mongoose";
 
 const app: Application = express();
+app.use(express.json());
 
 const noteSchema = new Schema({
   title: {type: String, required: true, trim: true},
@@ -20,22 +21,62 @@ const noteSchema = new Schema({
 
 const Note = model("Note", noteSchema);
 
-app.post('/create-note', async (req: Request, res: Response) => {
-  const myNote = new Note({
-    title: "Learning Next Level Dev",
-    content: "I'm learning next level web development",
-    tags: {
-      label: 'panding'
-    }
-  });
+app.post('/notes/create-note', async (req: Request, res: Response) => {
+  const body = req.body;
+  // hard-coded approch
+  // const myNote = new Note({
+  //   title: "Learning Next Level Dev",
+  //   content: "I'm learning next level web development",
+  //   tags: {
+  //     label: 'panding'
+  //   }
+  // });
+  // await myNote.save();
 
-  await myNote.save();
+  const note = await Note.create(body);
 
   res.status(201).json({
     success: true,
     message: "Note created successfully",
-    note: myNote,
+    note,
   })
+})
+
+app.get('/notes', async (req: Request, res: Response) => {
+  const notes = await Note.find();
+
+  res.status(201).json({
+    success: true,
+    message: "Get Notes",
+    notes
+  })
+})
+
+app.get('/notes/:noteId', async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const note = await Note.findById(noteId);
+  // const note = await Note.findOne({_id: noteId})
+
+  res.status(201).json({
+    success: true,
+    message: "Get Single Note",
+    note
+  })
+})
+
+app.patch('/notes/:noteId', async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const updatedBody = req.body;
+
+  const note = await Note.findByIdAndUpdate(noteId, updatedBody, {new: true})
+  // const note = await Note.updateOne({_id: noteId}, updatedBody, {new: true})
+
+  res.status(201).json({
+    success: true,
+    message: "Note Updated Successfully",
+    note
+  })
+
 })
 
 app.get('/', (req: Request, res:Response) => {
