d6efbaaf4b4da2aeb0432bc9dbe2140eebc70fd0
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-17 17:01:00 +0600

    implement custom email validator

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,10 +1,11 @@
 import { model, Schema } from "mongoose";
+import validator from 'validator';
 import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema<IUser>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, "firstName is required"],
     trim: true,
     minlength: 3,
     maxlength: 10,
@@ -19,12 +20,29 @@ const userSchema = new Schema<IUser>({
   email: {
     type: String,
     lowercase: true,
-    unique: true,
+    unique: [true, "This email is already exists"],
     required: true,
     trim: true,
+    // validate: {
+    //   validator: function (value) {
+    //     return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+    //   },
+    //   message: function (props) {
+    //     return `Email ${props.value} is not valid email`;
+    //   },
+    // },
+    validate: [validator.isEmail, 'Invalid Email address {VALUE}']
+
   },
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
+  role: {
+    type: String,
+    enum: {
+      values: ["user", "admin"],
+      message: "Role is not valid. got {VALUE}",
+    },
+    default: "user",
+  },
 });
 
 export const User = model("User", userSchema);
