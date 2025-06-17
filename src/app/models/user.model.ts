fb83663a4a68c51888d0098fa076a7e0b97ba8d8
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-17 16:35:13 +0600

    implement some validation

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,11 +2,29 @@ import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema<IUser>({
-  firstName: { type: String, required: true, trim: true },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 10,
+  },
   lastName: { type: String, required: true, trim: true },
-  email: { type: String, required: true, trim: true },
+  age: {
+    type: Number,
+    required: true,
+    min: [18, "Age must be at least 18 got {VALUE}"],
+    max: 60,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true,
+    trim: true,
+  },
   password: { type: String, required: true },
   role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
-export const User = model('User', userSchema)
+export const User = model("User", userSchema);
