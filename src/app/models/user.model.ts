b026700810216d0e7c8a13aba0b74572489dcd30
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-06-17 20:10:06 +0600

    implement embedding

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,48 +1,60 @@
 import { model, Schema } from "mongoose";
-import validator from 'validator';
-import { IUser } from "../interfaces/user.interface";
+import validator from "validator";
+import { IAddress, IUser } from "../interfaces/user.interface";
 
-const userSchema = new Schema<IUser>({
-  firstName: {
-    type: String,
-    required: [true, "firstName is required"],
-    trim: true,
-    minlength: 3,
-    maxlength: 10,
+const addressSchema = new Schema<IAddress>(
+  {
+    city: { type: String },
+    street: { type: String },
+    zip: { type: Number },
   },
-  lastName: { type: String, required: true, trim: true },
-  age: {
-    type: Number,
-    required: true,
-    min: [18, "Age must be at least 18 got {VALUE}"],
-    max: 60,
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    unique: [true, "This email is already exists"],
-    required: true,
-    trim: true,
-    // validate: {
-    //   validator: function (value) {
-    //     return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
-    //   },
-    //   message: function (props) {
-    //     return `Email ${props.value} is not valid email`;
-    //   },
-    // },
-    validate: [validator.isEmail, 'Invalid Email address {VALUE}']
+  { _id: false }
+);
 
-  },
-  password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: {
-      values: ["user", "admin"],
-      message: "Role is not valid. got {VALUE}",
+const userSchema = new Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      required: [true, "firstName is required"],
+      trim: true,
+      minlength: 3,
+      maxlength: 10,
     },
-    default: "user",
+    lastName: { type: String, required: true, trim: true },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "Age must be at least 18 got {VALUE}"],
+      max: 60,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: [true, "This email is already exists"],
+      required: true,
+      trim: true,
+      // validate: {
+      //   validator: function (value) {
+      //     return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+      //   },
+      //   message: function (props) {
+      //     return `Email ${props.value} is not valid email`;
+      //   },
+      // },
+      validate: [validator.isEmail, "Invalid Email address {VALUE}"],
+    },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: {
+        values: ["user", "admin"],
+        message: "Role is not valid. got {VALUE}",
+      },
+      default: "user",
+    },
+    address: { type: addressSchema },
   },
-});
+  { versionKey: false, timestamps: true }
+);
 
 export const User = model("User", userSchema);
